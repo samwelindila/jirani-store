f6418a800f7bc25e6073eb70b840d30ffb3c8a10
samwelindila81@gmail.com <ndillatech@gmail.com>
2025-11-23 11:57:45 +0300

    address model

--- models/Address.js
+++ models/Address.js
@@ -1,16 +1,15 @@
 import mongoose from "mongoose";
 
 const addressSchema = new mongoose.Schema({
-    userId: { type: String, required: true }, 
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }, 
     fullName: { type: String, required: true }, 
     phoneNumber: { type: String, required: true },
     pincode: { type: Number, required: true }, 
     area: { type: String, required: true },
-    city: {type: String, required: true},
-    state: {type: String, required:true },
-})
+    city: { type: String, required: true },
+    state: { type: String, required:true },
+});
 
+const Address = mongoose.models.Address || mongoose.model("Address", addressSchema);
 
-const Address = mongoose.models.address || mongoose.model('address' ,addressSchema);
-
-export default Address
+export default Address;
